03e3c77883c1dfa900b08f4dce200ab178bc84c4
Rubén Osorio López <Rubn Osorio Lpez>
2016-08-03 22:37:42 +0200

    Added initial files with scripts to call the shaders in both game and real resolutions. :)

---
+++ ShadedPixelCamera/Scripts/highResShaderCalls.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class highResShaderCalls : MonoBehaviour {
+
+    public float intensity;
+    private Material material;
+
+    // Creates a private material used to the effect
+    void Awake()
+    {
+        material = new Material(Shader.Find("Hidden/ntsc"));
+    }
+
+    // Postprocess the image
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Debug.Log("HighResShader: Source width and height: (" + source.width + ","+source.height+")");
+        
+
+        if (intensity == 0)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
+        material.SetFloat("_bwBlend", intensity);
+        Graphics.Blit(source, destination, material);
+
+
+        if (destination != null)
+        {
+            Debug.Log(destination.height);
+            Debug.Log(destination.width);
+        }
+    }
+
+
+}
